f6402f52c4b518cfcc62f8a9759510ea1a199199
OM ARGADE <126243997+OmArgade09@users.noreply.github.com>
2024-11-29 11:17:46 +0530

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,15 @@
+class Operations {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        int mul = a * b;
+        int div = b / a;
+
+        System.out.println(sum);
+        System.out.println(mul);
+        System.out.println(div);
+
+    }
+}
